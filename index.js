cc967b81e48a1daf71930b475eee57507be01292
JhowDarks <yaneduardo123@gmail.com>
2021-03-31 11:16:53 -0300

    firebase add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const config = require('./config.json')
-const alarm =  require('alarm')
+const alarm = require('alarm')
 const fetch = require('node-fetch')
 
 // Criando objeto de discord (client method)
@@ -22,10 +22,10 @@ const start = Date.now()
 client.login(config.BOT_TOKEN)
 
 // Client on como um listener para mensagens
-client.on('message', function(message) {
+client.on('message', function (message) {
     //Check se mensagem foi enviada por bot
     if (message.author.bot) return
-    
+
     //Check se a mensagem nao comeca com o prefixo declarado
     if (!message.content.startsWith(prefix)) return
 
@@ -35,14 +35,14 @@ client.on('message', function(message) {
 
     // Removendo o prefixo da mensagem
     const commandBody = message.content.slice(prefix.length)
-    
+
     // Pegando argumentos passados para comando e adicionando a array
     const args = commandBody.split(' ')
-    
+
     // Tirando primeiro elemento da array, botando em minusculo e retornando como "command" 
     const command = args.shift().toLowerCase()
 
-    if(command == 'test'){
+    if (command == 'test') {
         // notifyClock(lucas_id)
         message.author.send(`Your user: ${message.author}`)
     }
@@ -51,32 +51,36 @@ client.on('message', function(message) {
 
 //////////////////////////TEST//////////////////////////
 // Criando tempo de execucao 2 segundos apos o tempo de inicio
-timeForExec = new Date(+start + 2000)
+
+var timeForExec = new Date()
 console.log(timeForExec)
+if (timeForExec.getDate() == 0 || timeForExec.getDate() == 6) {
+    timeForExec.setHours(13, 0, 0)
 
-// alarm(timeForExec, function () {
-//     notifyClock(yan_id)
-// });
+    alarm(timeForExec, function () {
+        notifyClock(renan_id)
+    });
+}
 
 
-alarm.recurring(3000, function() {
-    notifyClock(yan_id)
-})
+//alarm.recurring(3000, function() {
+//    notifyClock(yan_id)
+//})
 
 
 /**
  * Funcao para enviar DM's para notificar os usuarios para bater ponto.
  * @param {*} user_id
  */
-function notifyClock(user_id){
-    try{
+function notifyClock(user_id) {
+    try {
         client.users.fetch(user_id)
-        .then((user) => {
-            console.log(user)
-            user.send('Vem fazer o software krai')
-            return user
-        })
-    }catch(error){
+            .then((user) => {
+                console.log(user)
+                user.send('salve')
+                return user
+            })
+    } catch (error) {
         console.error(error);
     }
 }
